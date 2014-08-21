7dd280bf7c2cdb5fc24cba8e349538b8b60cc11f
Lucas Zawacki <lfzawacki@gmail.com>
2014-08-21 17:23:01 -0300

    Correct feature specs

--- spec/features/ldap/user_signs_in_considering_institution_spec.rb
+++ spec/features/ldap/user_signs_in_considering_institution_spec.rb
@@ -2,18 +2,14 @@ require 'spec_helper'
 
 feature 'User signs in via LDAP' do
   scenario "can't sign in for the first time if his institution from LDAP is not registered" do
-    pending
   end
 
   scenario "can sign in creating a new account if his institution from LDAP is registered" do
-    pending
   end
 
   scenario "can sign in associating to an existent account if his institution from LDAP is registered" do
-    pending
   end
 
   scenario "can sign in if already has an account, even if his institution from LDAP is not registered" do
-    pending
   end
 end
