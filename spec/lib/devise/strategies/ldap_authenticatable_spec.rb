5f4edee3dc287eace93a3ba3e59e2466c706f34e
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-06-03 12:39:20 -0300

    Allow sign in using email on LDAP
    Was accepting username only, now it accepts emails also. More consistent
    with how the local sign in works.

--- spec/lib/devise/strategies/ldap_authenticatable_spec.rb
+++ spec/lib/devise/strategies/ldap_authenticatable_spec.rb
@@ -285,11 +285,12 @@ describe Devise::Strategies::LdapAuthenticatable do
       let(:configs) {
         u = Object.new
         u.stub(:ldap_username_field).and_return("username")
+        u.stub(:ldap_email_field).and_return("mail")
         u.stub(:ldap_filter).and_return("(&(objectclass=user)(objectcategory=person))")
         u
       }
       let(:expected) {
-        Net::LDAP::Filter.construct("(&(&(objectclass=user)(objectcategory=person))(username=user-login))")
+        Net::LDAP::Filter.construct("(&(&(objectclass=user)(objectcategory=person))(|(mail=user-login)(username=user-login)))")
       }
       before { target.stub(:params).and_return(params) }
       it { target.ldap_filter(configs).should eq(expected) }
@@ -300,11 +301,12 @@ describe Devise::Strategies::LdapAuthenticatable do
       let(:configs) {
         u = Object.new
         u.stub(:ldap_username_field).and_return("username")
+        u.stub(:ldap_email_field).and_return("mail")
         u.stub(:ldap_filter).and_return(nil)
         u
       }
       let(:expected) {
-        Net::LDAP::Filter.construct("(username=user-login)")
+        Net::LDAP::Filter.construct("(|(mail=user-login)(username=user-login))")
       }
       before { target.stub(:params).and_return(params) }
       it { target.ldap_filter(configs).should eq(expected) }
@@ -315,11 +317,12 @@ describe Devise::Strategies::LdapAuthenticatable do
       let(:configs) {
         u = Object.new
         u.stub(:ldap_username_field).and_return("username")
+        u.stub(:ldap_email_field).and_return("mail")
         u.stub(:ldap_filter).and_return("anything weird and invalid")
         u
       }
       let(:expected) {
-        Net::LDAP::Filter.construct("(username=user-login)")
+        Net::LDAP::Filter.construct("(|(mail=user-login)(username=user-login))")
       }
       before { target.stub(:params).and_return(params) }
       it { target.ldap_filter(configs).should eq(expected) }
@@ -330,11 +333,12 @@ describe Devise::Strategies::LdapAuthenticatable do
       let(:configs) {
         u = Object.new
         u.stub(:ldap_username_field).and_return("username")
+        u.stub(:ldap_email_field).and_return("mail")
         u.stub(:ldap_filter).and_return("")
         u
       }
       let(:expected) {
-        Net::LDAP::Filter.construct("(username=user-login)")
+        Net::LDAP::Filter.construct("(|(mail=user-login)(username=user-login))")
       }
       before { target.stub(:params).and_return(params) }
       it { target.ldap_filter(configs).should eq(expected) }
