b2baf58d96cad8156b8eabc95e0745f55fadb30b
Lucas Zawacki <lfzawacki@gmail.com>
2015-07-06 15:48:17 -0300

    Tests for institution based space moderation

--- spec/features/spaces/creating_a_space_with_institutions_spec.rb
+++ spec/features/spaces/creating_a_space_with_institutions_spec.rb
@@ -21,101 +21,117 @@ feature "Creating a space when institutions are present" do
   let!(:user) { FactoryGirl.create(:user) }
   let(:institution) { FactoryGirl.create(:institution) }
 
-  feature "creating a space as a normal user" do
+  context "creating a space as a normal user" do
 
-    scenario "in a non moderated institution" do
-      user.update_attributes(institution: institution)
+    context "in a non moderated institution" do
+      let(:attrs) { FactoryGirl.attributes_for(:space, public: true, disabled: false) }
+      before {
+        user.update_attributes(institution: institution)
+        login_as(user, :scope => :user)
+        create_space_from_attrs(attrs)
+      }
 
-      attrs = FactoryGirl.attributes_for(:space, public: true)
-      login_as(user, :scope => :user)
-
-      create_space_from_attrs(attrs)
-
-      Space.last.should be_approved
-      current_path.should eq(space_path(Space.last))
-      page.should have_content(attrs[:name])
+      it { Space.last.should be_approved }
+      it { current_path.should eq(space_path(Space.last)) }
+      it { page.should have_content(attrs[:name]) }
     end
 
-    scenario "in a moderated institution" do
-      user.update_attributes(institution: institution)
-      institution.update_attributes(require_space_approval: true)
+    context "in a moderated institution" do
+      let(:attrs) { FactoryGirl.attributes_for(:space) }
+      before {
+        user.update_attributes(institution: institution)
+        institution.update_attributes(require_space_approval: true)
+        login_as(user, :scope => :user)
+        create_space_from_attrs(attrs)
 
-      login_as(user, :scope => :user)
+      }
 
-      attrs = FactoryGirl.attributes_for(:space)
-      create_space_from_attrs(attrs)
+      it { Space.last.should_not be_approved }
+      it { current_path.should eq(spaces_path) }
+      it { has_success_message t('space.created_waiting_moderation') }
 
-      Space.last.should_not be_approved
-      current_path.should eq(spaces_path)
-      has_success_message t('space.created_waiting_moderation')
+      it { page.should_not have_content(attrs[:name]) }
 
-      page.should_not have_content(attrs[:name])
+      context 'space appears in user spaces' do
+        before { visit spaces_path(my_spaces: 'true') }
 
-      visit spaces_path(my_spaces: 'true')
-      page.should have_content(attrs[:name])
-      page.should have_selector('.waiting-approval', count: 1)
-      page.should have_selector('.icon-mconf-waiting-moderation', count: 1)
+        it { page.should have_content(attrs[:name]) }
+        it { page.should have_selector('.waiting-approval', count: 1) }
+        it { page.should have_selector('.icon-mconf-waiting-moderation', count: 1) }
+      end
     end
 
-    scenario "in an institution with space creation forbidden" do
-      user.update_attributes(institution: institution)
-      institution.update_attributes(forbid_user_space_creation: true)
+    context "in an institution with space creation forbidden" do
+      before {
+        user.update_attributes(institution: institution)
+        institution.update_attributes(forbid_user_space_creation: true)
 
-      login_as(user, :scope => :user)
+        login_as(user, :scope => :user)
+        visit spaces_path
+      }
 
-      visit spaces_path
-      expect{ find_link('', href: new_space_path) }.to  raise_error
+      it { expect{ find_link('', href: new_space_path) }.to  raise_error }
 
-      visit new_space_path
+      context 'cant create a space' do
+        before { visit new_space_path }
 
-      current_path.should eq(spaces_path)
-      has_failure_message t('spaces.error.creation_forbidden')
+        it { current_path.should eq(spaces_path) }
+        it { has_failure_message t('spaces.error.creation_forbidden') }
+      end
     end
 
   end
 
   feature "creating a space as an institution admin" do
 
-    scenario "in a non moderated institution" do
-      user.update_attributes(institution: institution)
-      user.permissions.last.update_attributes(role: Role.where(name: 'Admin').first)
+    context "in a non moderated institution" do
+      let(:attrs) { FactoryGirl.attributes_for(:space, public: true) }
+      before {
+        user.update_attributes(institution: institution)
+        user.permissions.last.update_attributes(role: Role.where(name: 'Admin').first)
 
-      attrs = FactoryGirl.attributes_for(:space)
-      login_as(user, :scope => :user)
+        login_as(user, :scope => :user)
 
-      create_space_from_attrs(attrs)
+        create_space_from_attrs(attrs)
+      }
 
-      Space.last.should be_approved
-      current_path.should eq(space_path(Space.last))
-      page.should have_content(attrs[:name])
+      it { Space.last.should be_approved }
+      it { current_path.should eq(space_path(Space.last)) }
+      it { page.should have_content(attrs[:name]) }
     end
 
-    scenario "in a moderated institution" do
-      user.update_attributes(institution: institution)
-      institution.update_attributes(require_space_approval: true)
-      user.permissions.last.update_attributes(role: Role.where(name: 'Admin').first)
+    context "in a moderated institution" do
+      let(:attrs) { FactoryGirl.attributes_for(:space, public: true) }
+      before {
+        user.update_attributes(institution: institution)
+        institution.update_attributes(require_space_approval: true)
+        user.permissions.last.update_attributes(role: Role.where(name: 'Admin').first)
+        login_as(user, :scope => :user)
 
-      attrs = FactoryGirl.attributes_for(:space)
-      login_as(user, :scope => :user)
+        create_space_from_attrs(attrs)
+      }
 
-      create_space_from_attrs(attrs)
-
-      Space.last.should be_approved
-      current_path.should eq(space_path(Space.last))
-      page.should have_content(attrs[:name])
+      it { Space.last.should be_approved }
+      it { current_path.should eq(space_path(Space.last)) }
+      it { page.should have_content(attrs[:name]) }
     end
 
-    scenario "in an institution with space creation forbidden" do
-      user.update_attributes(institution: institution)
-      institution.update_attributes(forbid_user_space_creation: true)
-      user.permissions.last.update_attributes(role: Role.where(name: 'Admin').first)
-      login_as(user, :scope => :user)
+    context "in an institution with space creation forbidden" do
+      before {
+        user.update_attributes(institution: institution)
+        institution.update_attributes(forbid_user_space_creation: true)
+        user.permissions.last.update_attributes(role: Role.where(name: 'Admin').first)
+        login_as(user, :scope => :user)
 
-      visit spaces_path
-      page.find_link('', href: new_space_path).should be_visible
+        visit spaces_path
+      }
 
-      visit new_space_path
-      current_path.should eq(new_space_path)
+      it { page.find_link('', href: new_space_path).should be_visible }
+
+      context 'can create a space' do
+        before { visit new_space_path }
+        it { current_path.should eq(new_space_path) }
+      end
     end
 
   end
